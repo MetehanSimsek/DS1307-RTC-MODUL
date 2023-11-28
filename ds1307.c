55b43df50f7d685a71adf8d2aeb0aa20b6357950
MetehanSimsek <71407793+MetehanSimsek@users.noreply.github.com>
2023-11-28 03:23:46 +0300

    Add files via upload

--- ds1307.c
+++ ds1307.c
@@ -1,84 +1,177 @@
 #include "ds1307.h"
-#include "pcd8544.h"
-#include "stdio.h"
+#include <stm32f1xx_hal.h>
+#include <stddef.h>
+#include <stdio.h>
 
+#define  RTC_WRITE_ADRESS       (0xD0)
+#define  RTC_READ_ADRESS        (0xD1)
+#define  SECOND_ADDR            (0x00)
+#define  MINUTES_ADDR           (0x01)
+#define  HOURS_ADDR             (0x02)
+#define  DAY_ADDR               (0x03)
+#define  DATE_ADDR              (0x04)
+#define  MONTH_ADDR             (0x05)
+#define  YEAR_ADDR              (0x06)
+#define  ONE_BYTE               (0x01)
+#define  EEPROM_CLOCK_ADDR      (0x00)
+#define  RTC_SET_OPEN           (0x00)
+#define  RTC_SET_CLOSED         (0x01)
 
-#define  RTC_Write_Adress       0xD0
-#define  RTC_Read_Adress        0xD1
-#define  Second_addr            0x00
-#define  Minutes_addr           0x01
-#define  Hours_addr             0x02
-#define  Day_addr               0x03
-#define  Date_addr              0x04
-#define  Month_addr             0x05
-#define  Year_addr              0x06
-#define  Control_addr           0x07
-#define  RAM_first_addr         0x08
-
-#define  AM_12Hours             0x00
-#define  PM_12Hours             0x01
-#define  Format24h              0x02
-
-#define  One_byte               0x01
-
-#define Clock_Adress            0x00
-
-I2C_HandleTypeDef hi2c1;
-
-typedef struct {
-  uint8_t sec;
-  uint8_t min;
-  uint8_t hour;
-}rtc_time;
-
-typedef struct {
-  uint8_t  day;
-  uint8_t  date;
-  uint8_t  month;
-  uint8_t  year;
-}rtc_date;
-
+static I2C_HandleTypeDef hi2c1;
 static int bcd_to_dec(uint8_t variable);
+static int read_to_eeprom(uint16_t eeprom_addr);
+static int write_to_eeprom(uint8_t send_data, uint16_t eeprom_addr);
 
-rtc_time time;
-rtc_date date;
 
+rtc_time time_val;
 uint8_t receive_variable;
 uint8_t memory;
 
-int write_to_eeprom(uint8_t send_data, uint16_t eeprom_addr)
+
+void ds1307_time_set(uint8_t sec_value, uint8_t min_value, uint8_t hour_value, uint8_t day_value, uint8_t month_value, uint8_t year_value, uint8_t date_value)
+{
+  memory = read_to_eeprom(EEPROM_CLOCK_ADDR);
+  if(memory == 0)
+  {
+    time_val.sec = sec_value;
+    set_sec(time_val.sec);     
+    
+    time_val.min = min_value; 
+    set_min(time_val.min);
+    
+    time_val.hour = hour_value;
+    set_hour(time_val.hour);
+
+    time_val.date = date_value;  
+    set_date(time_val.date);        
+    
+    time_val.month = month_value;
+    set_month(time_val.month);       
+    
+    time_val.year =  year_value;
+    set_year(time_val.year);      
+    
+    time_val.day = date_value; 
+    set_day(time_val.day);    
+    write_to_eeprom(RTC_SET_CLOSED,EEPROM_CLOCK_ADDR);
+  }
+}
+
+
+void ds1307_get_time(void)
+{
+  HAL_I2C_Mem_Read(&hi2c1,RTC_READ_ADRESS,(uint16_t)SECOND_ADDR,ONE_BYTE,&(time_val.sec),ONE_BYTE,1);
+  time_val.sec = bcd_to_dec(time_val.sec);
+  HAL_I2C_Mem_Read(&hi2c1,RTC_READ_ADRESS,(uint16_t)MINUTES_ADDR,ONE_BYTE,&(time_val.min),ONE_BYTE,1);
+  time_val.min = bcd_to_dec(time_val.min);
+  HAL_I2C_Mem_Read(&hi2c1,RTC_READ_ADRESS,(uint16_t)HOURS_ADDR,ONE_BYTE,&(time_val.hour),ONE_BYTE,1);
+  time_val.hour = bcd_to_dec(time_val.hour);
+  HAL_I2C_Mem_Read(&hi2c1,RTC_READ_ADRESS,(uint16_t)DAY_ADDR,ONE_BYTE,&(time_val.day),ONE_BYTE,1);
+  time_val.day = bcd_to_dec(time_val.day);
+  HAL_I2C_Mem_Read(&hi2c1,RTC_READ_ADRESS,(uint16_t)MONTH_ADDR,ONE_BYTE,&(time_val.month),ONE_BYTE,1);
+  time_val.month = bcd_to_dec(time_val.month);
+  HAL_I2C_Mem_Read(&hi2c1,RTC_READ_ADRESS,(uint16_t)YEAR_ADDR,ONE_BYTE,&(time_val.year),ONE_BYTE,1);
+  time_val.year = bcd_to_dec(time_val.year);
+  HAL_I2C_Mem_Read(&hi2c1,RTC_READ_ADRESS,(uint16_t)DATE_ADDR,ONE_BYTE,&(time_val.date),ONE_BYTE,1);
+  time_val.date = bcd_to_dec(time_val.date);
+}
+
+
+void ds1307_time_calibrate(uint8_t sec_value, uint8_t min_value, uint8_t hour_value, uint8_t day_value, uint8_t month_value, uint8_t year_value, uint8_t date_value)
+{
+  write_to_eeprom(RTC_SET_OPEN,EEPROM_CLOCK_ADDR);
+  ds1307_time_set(sec_value, min_value, hour_value, day_value, month_value, year_value, date_value);
+}
+
+
+void set_sec(uint8_t sec_value)
+{
+  time_val.sec = sec_value;
+  HAL_I2C_Mem_Write(&hi2c1,(uint16_t)RTC_READ_ADRESS,(uint16_t)SECOND_ADDR,(uint16_t)ONE_BYTE,&time_val.sec,(uint16_t)ONE_BYTE,HAL_MAX_DELAY);
+}
+
+
+void set_min(uint8_t min_value)
+{
+  time_val.sec = min_value;
+  HAL_I2C_Mem_Write(&hi2c1,(uint16_t)RTC_WRITE_ADRESS,(uint16_t)MINUTES_ADDR,(uint16_t)ONE_BYTE,&time_val.min,(uint16_t)ONE_BYTE,HAL_MAX_DELAY);
+}
+
+
+void set_hour(uint8_t hour_value)
+{
+  time_val.hour = hour_value;
+  HAL_I2C_Mem_Write(&hi2c1,(uint16_t)RTC_WRITE_ADRESS,(uint16_t)HOURS_ADDR,(uint16_t)ONE_BYTE,&time_val.hour,(uint16_t)ONE_BYTE,HAL_MAX_DELAY);
+}
+
+
+void set_day(uint8_t day_value)
+{
+  time_val.date = day_value;
+  HAL_I2C_Mem_Write(&hi2c1,(uint16_t)RTC_WRITE_ADRESS,(uint16_t)DAY_ADDR,(uint16_t)ONE_BYTE,&time_val.day,(uint16_t)ONE_BYTE,HAL_MAX_DELAY);
+}
+
+
+void set_month(uint8_t month_value)
+{
+  time_val.month = month_value;
+  HAL_I2C_Mem_Write(&hi2c1,(uint16_t)RTC_WRITE_ADRESS,(uint16_t)MONTH_ADDR,(uint16_t)ONE_BYTE,&time_val.date,(uint16_t)ONE_BYTE,HAL_MAX_DELAY); 
+}
+
+
+void set_year(uint8_t year_value)
+{
+  time_val.year = year_value;
+  HAL_I2C_Mem_Write(&hi2c1,(uint16_t)RTC_WRITE_ADRESS,(uint16_t)YEAR_ADDR,(uint16_t)ONE_BYTE,&time_val.year,(uint16_t)ONE_BYTE,HAL_MAX_DELAY);
+}
+
+
+void set_date(uint8_t date_value)
+{
+  time_val.date = date_value;
+  HAL_I2C_Mem_Write(&hi2c1,(uint16_t)RTC_WRITE_ADRESS,(uint16_t)DATE_ADDR,(uint16_t)ONE_BYTE,&time_val.date,(uint16_t)ONE_BYTE,HAL_MAX_DELAY); 
+}
+
+
+void ds1307init(void)
+{
+  hi2c1.Instance = I2C1;
+  hi2c1.Init.ClockSpeed = 100000;
+  hi2c1.Init.DutyCycle = I2C_DUTYCYCLE_2;
+  hi2c1.Init.OwnAddress1 = 0;
+  hi2c1.Init.AddressingMode = I2C_ADDRESSINGMODE_7BIT;
+  hi2c1.Init.DualAddressMode = I2C_DUALADDRESS_DISABLE;
+  hi2c1.Init.OwnAddress2 = 0;
+  hi2c1.Init.GeneralCallMode = I2C_GENERALCALL_DISABLE;
+  hi2c1.Init.NoStretchMode = I2C_NOSTRETCH_DISABLE;
+  HAL_I2C_Init(&hi2c1);
+}
+
+
+/**
+  * @brief Read value to EEPROM
+  * @param eeprom_addr where the data will be read
+  * @retval receive_variable;
+  */
+static int read_to_eeprom(uint16_t eeprom_addr)
+{
+  HAL_I2C_Mem_Read(&hi2c1,0xA1,eeprom_addr,2,&receive_variable,2,HAL_MAX_DELAY);
+  return receive_variable;
+}
+
+
+/**
+  * @brief Write value to EEPROM
+  * @param data variable is sent to eeprom
+  * @param eeprom_addr where the data will be sent
+  * @retval send_data;
+  */
+static int write_to_eeprom(uint8_t send_data, uint16_t eeprom_addr)
 {
   HAL_I2C_Mem_Write(&hi2c1,0xA0,eeprom_addr,2,&send_data,2,HAL_MAX_DELAY);
   return send_data;
 }
 
-int read_to_eeprom(uint16_t eeprom_addr)
-{
-
-  HAL_I2C_Mem_Read(&hi2c1,0xA1,eeprom_addr,2,&receive_variable,2,HAL_MAX_DELAY);
-  return receive_variable;
-}
-
-void get_time(void)
-{
-  char ptr_sec[3], ptr_min[3], ptr_hour[3];
-
-  HAL_I2C_Mem_Read(&hi2c1,RTC_Read_Adress,(uint16_t)Second_addr,One_byte,&(time.sec),One_byte,1);
-  time.sec = bcd_to_dec(time.sec);
-  HAL_I2C_Mem_Read(&hi2c1,RTC_Read_Adress,(uint16_t)Minutes_addr,One_byte,&(time.min),One_byte,1);
-  time.min = bcd_to_dec(time.min);
-  HAL_I2C_Mem_Read(&hi2c1,RTC_Read_Adress,(uint16_t)Hours_addr,One_byte,&(time.hour),One_byte,1);
-  time.hour = bcd_to_dec(time.hour);
-  
-  sprintf(ptr_sec,"%02d",time.sec);
-  sprintf(ptr_min,"%02d",time.min);
-  sprintf(ptr_hour,"%02d",time.hour);
-  lcdPrint(ptr_hour,0,0);
-  lcdPrint(":",12,0);
-  lcdPrint(ptr_min,17,0);
-  lcdPrint(":",29,0);
-  lcdPrint(ptr_sec,34,0);
-}
 
 /**
   * @brief Converts BCD value to DECIMAL
@@ -91,55 +184,4 @@ static int bcd_to_dec(uint8_t variable)
     dec_value += (variable & 0x0F); 
     dec_value += ((variable >> 4) & 0x0F) * 10; 
     return dec_value;
-}
-
-void ds1307_tim_date_set(uint8_t _sec, uint8_t _min, uint8_t _hour, uint8_t _day, uint8_t _month, uint8_t _year, uint8_t _date)
-{
-  
-  memory = read_to_eeprom(Clock_Adress);
-  if(memory == 0)
-  {
-    time.sec = _sec;
-    set_sec(time.sec);     
-    
-    time.min = _min; 
-    set_min(time.min);
-    
-    time.hour = _hour;
-    set_hour(time.hour);
-    
-    date.day = _day; 
-    set_day(date.day);      
-    
-    date.date = _date;  
-    set_date(date.date);        
-    
-    date.month = _month;
-    set_month(date.month);       
-    
-    date.year =  _year;
-    set_year(date.year);      
-    
-    write_to_eeprom(0x01,Clock_Adress);
-  }
-}
-
-
-void I2C1_Init(void)
-{
-
-  hi2c1.Instance = I2C1;
-  hi2c1.Init.ClockSpeed = 100000;
-  hi2c1.Init.DutyCycle = I2C_DUTYCYCLE_2;
-  hi2c1.Init.OwnAddress1 = 0;
-  hi2c1.Init.AddressingMode = I2C_ADDRESSINGMODE_7BIT;
-  hi2c1.Init.DualAddressMode = I2C_DUALADDRESS_DISABLE;
-  hi2c1.Init.OwnAddress2 = 0;
-  hi2c1.Init.GeneralCallMode = I2C_GENERALCALL_DISABLE;
-  hi2c1.Init.NoStretchMode = I2C_NOSTRETCH_DISABLE;
-  if (HAL_I2C_Init(&hi2c1) != HAL_OK)
-  {
-    Error_Handler();
-  }
-
 }
--- ds1307.h
+++ ds1307.h
@@ -1,4 +1,3 @@
-#pragma once
 #ifndef __DS1307_H
 #define __DS1307_H
 
@@ -6,15 +5,26 @@
 extern "C" {
 #endif
   
-#include "main.h"  
+#include <stdint.h>
+#include <stddef.h>
+typedef struct {
+  uint8_t sec;
+  uint8_t min;
+  uint8_t hour;
+  uint8_t day;
+  uint8_t date;
+  uint8_t month;
+  uint8_t year;
+}rtc_time;
 
-  
+//rtc_time time;
 /**
-  * @brief I2C1 Initialization Function
+  * @brief Initialize ds1307 
   * @param None
   * @retval None
   */  
-void I2C1_Init(void);
+void ds1307init(void);
+
 
 /** @brief ds1307_tim_date_set adjust  RTC time
   * @param sec is parameter to set second
@@ -26,29 +36,78 @@ void I2C1_Init(void);
   * @param date is parameter to set date
   * @retval None
 **/
-void ds1307_tim_date_set(uint8_t sec, uint8_t min, uint8_t hour, uint8_t day, uint8_t month, uint8_t year, uint8_t date);
+void ds1307_time_set(uint8_t sec_value, uint8_t min_value, uint8_t hour_value, uint8_t day_value, uint8_t month_value, uint8_t year_value, uint8_t date_value);
+
+
+/** @brief ds1307_time_calibrate callibrate  RTC time
+  * @param sec is parameter to set second
+  * @param min is parameter to set min
+  * @param hour is parameter to set hour
+  * @param day is parameter to set day
+  * @param month is parameter to set month
+  * @param year is parameter to set year
+  * @param date is parameter to set date
+  * @retval None
+**/
+void ds1307_time_calibrate(uint8_t sec_value, uint8_t min_value, uint8_t hour_value, uint8_t day_value, uint8_t month_value, uint8_t year_value, uint8_t date_value);
+
 
 /**
-  * @brief Print PCD8544 screen time
+  * @brief ds1307_get_time shows current time value in time_val
   * @param None
   * @retval None
   */
-void get_time(void);
+void ds1307_get_time(void);
 
-/**
-  * @brief Write value to EEPROM
-  * @param data variable is sent to eeprom
-  * @param eeprom_addr where the data will be sent
-  * @retval send_data;
-  */
-int write_to_eeprom(uint8_t send_data, uint16_t eeprom_addr);
 
-/**
-  * @brief Read value to EEPROM
-  * @param eeprom_addr where the data will be read
-  * @retval receive_variable;
-  */
-int read_to_eeprom(uint16_t eeprom_addr);
+/** @brief set_sec set second
+  * @param sec_value is parameter to set second
+  * @retval None
+**/
+void set_sec(uint8_t sec_value);
+
+
+/** @brief set_min set minute
+  * @param min_value is parameter to set minute
+  * @retval None
+**/
+void set_min(uint8_t min_value);
+
+
+/** @brief set_hour set hour
+  * @param hour_value is parameter to set hour
+  * @retval None
+**/
+void set_hour(uint8_t hour_value);
+
+
+/** @brief set_day set day
+  * @param day_value is parameter to set day
+  * @retval None
+**/
+void set_day(uint8_t day_value);
+
+
+/** @brief set_date set date 
+  * @param date_value is parameter to set date
+  * @retval None
+**/
+void set_date(uint8_t date_value);
+
+
+/** @brief set_month set month 
+  * @param month_value is parameter to set month
+  * @retval None
+**/
+void set_month(uint8_t month_value);
+
+
+/** @brief set_year set year 
+  * @param year_value is parameter to set year
+  * @retval None
+**/
+void set_year(uint8_t year_value);  
+
 
 #ifdef __cplusplus
 }
